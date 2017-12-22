496006393d9e8983b71065b61b719622762b407c
salinas-manuel <salinas.manuel_1980@yahoo.com>
2017-12-22 14:50:34 -0600

    Refactored code

--- jquery/dist/js/main.js
+++ jquery/dist/js/main.js
@@ -1,5 +1,3 @@
-//console.log(nasaKey);
-
 var apod = {
   // Create a random apodDate
   randomDate: function(start, end) {
@@ -24,45 +22,49 @@ var apod = {
     return `${y}-${m}-${d}`;
   },
 
-  //Application Constructor
-  init: function() {
+  buildDom: function(result){
+
+    $('#apodTitle').text(result.title);
+
+    if(result.media_type === 'video'){
+      $('#apodImg').hide();
+      $('#apodVideo > iframe').attr('src', result.url).show();
+    }else{
+      $('#apodVideo').hide();
+      $('#apodImg').attr('src', result.url).attr('alt', result.title).show();
+    }
+
+    $('#apodCopyright').text(result.copyright);
+    $('#apodDate').text(result.date);
+    $('#apodDesc').text(result.explanation);
+
+  },
+
+  getRequest: function(){
+    let _this = this;
     let date = this.randomDate(new Date(1995, 5, 16), new Date());
-    console.log(date);
-    var url = "https://api.nasa.gov/planetary/apod?api_key="
+    let url = "https://api.nasa.gov/planetary/apod?api_key="
      + nasaKey
      + '&date=' + date;
 
     $.ajax({
       url: url
     }).done(function(result){
-      console.log(result);
-
-      $('#apodTitle').text(result.title);
-      if(result.media_type === 'video'){
-        $('#apodImg').hide();
-        $('#apodVideo > iframe').attr('src', result.url).show();
-      }else{
-        $('#apodVideo').hide();
-        $('#apodImg').attr('src', result.url).attr('alt', result.title).show();
-      }
-
-      $('#apodCopyright').text(result.copyright);
-      $('#apodDate').text(result.date);
-      $('#apodDesc').text(result.explanation);
-
+      _this.buildDom(result);
     }).fail(function(result){
       console.log(result);
     });
-
   },
+  //Initialization method.
+  init: function() {
+    this.getRequest();
+  }
 };
-/*<h1 id="apodTitle"></h1>
-<img id="apodImg">
-<div class="video">
-  <iframe frameborder="0" allowfullscreen></iframe>
-</div>
-<div id="apodCopyright"></div>
-<div id="apodDate"></div>
-<div id="apodDesc"></div>
-*/
+
 apod.init();
+
+$(function(){
+  $('#btnRandom').on('click', function(){
+    apod.getRequest();
+  })
+});
